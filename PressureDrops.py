9765a87d8272cf620025be863fcdf65e7b6dfb8b
Patryk Slawinski-Slawecki <p.slawinski.s@gmail.com>
2022-08-11 21:54:04 +0200

    Dictionary consisting of nominal diameter designation and internal pipe diameter value pairs.

---
+++ PressureDrops.py
@@ -0,0 +1,13 @@
+import CoolProp.CoolProp as CP
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+
+DN = {"DN20": 21.6,             #Nominal diameters, mm
+      "DN32": 35.9,
+      "DN40": 41.8,
+      "DN50": 53.0,
+      "DN65": 68.8,
+      "DN80": 80.8,
+      "DN100": 105.3}
+
