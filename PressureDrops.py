Calculation errors corrected

PressureDrops.py
@@ -14,7 +14,7 @@ DN = {"DN20": 21.6,             #Nominal diameters, mm
 def dp(d_in, T, p, u, fluid): #(internal diameter, temperature, pressure, velocity, fluid)
       D_h = d_in / 1000 #hydraulic diameter, m
       ro = CP.PropsSI("D", "T", T, "P", p*10**5, fluid) #density, kg/m3
-      visc_kin = CP.PropsSI("VISCOSITY", "T", T, "P", p*10**5, fluid) #kinematic viscosity, m2/s
+      visc_kin = CP.PropsSI("VISCOSITY", "T", T, "P", p*10**5, fluid) / ro #kinematic viscosity, m2/s
       Re = D_h * u / visc_kin #Reynolds number, -
       if Re < 2300:
             lam = 64 / Re #friction factor, -
@@ -23,12 +23,12 @@ def dp(d_in, T, p, u, fluid): #(internal diameter, temperature, pressure, veloci
             Convergence = False
             while Convergence == False:
                   lam = (1 / (2 * math.log10(Re * math.sqrt(lam1)) - 0.8))**2
-                  error < abs(lam1 - lam) / lam
+                  error = abs(lam1 - lam) / lam
                   if error < 0.5:
                         Convergence = True
                   else:
                         lam1 = lam1 + 0.01
-      dp = lam * 1/d_in * u**2 / 2 * ro #pressure drops per 1m of pipe, Pa/m
+      dp = lam * 1/(d_in/1000) * u**2 / 2 * ro #pressure drops per 1m of pipe, Pa
       return(dp)
 
 for i in np.arange(1, 2, 0.1):